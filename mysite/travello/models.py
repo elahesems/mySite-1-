47f86f438077660deb8dc682d6c196a8904ac956
elahe sems <olenasems88@gmail.com>
2020-08-10 20:12:55 +0300

    sql

--- mysite/travello/models.py
+++ mysite/travello/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id: int
-    name: str
-    img: str
-    desc: str
-    price: int
+class Destination(models.Model):
+
+    name= models.CharField(max_length=50,null=True)
+    img= models.ImageField(upload_to='pics',null=True,blank=True)
+    desc= models.CharField(max_length=50,null=True)
+    price= models.IntegerField(null=True)
+    offer = models.BooleanField(default=False)
 
